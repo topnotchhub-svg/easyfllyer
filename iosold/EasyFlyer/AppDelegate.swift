7d37cb1ad3262e74ea43f81201e9a25bf90b5979
mailj <topnotchhub@gmail.com>
2026-04-10 09:45:34 +0530

    ios folder settings

---
+++ iosold/EasyFlyer/AppDelegate.swift
@@ -0,0 +1,32 @@
+import UIKit
+import React
+import React_RCTAppDelegate
+import FirebaseCore
+
+@main
+class AppDelegate: RCTAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+  ) -> Bool {
+    // Configure Firebase before anything else
+    FirebaseApp.configure()
+    
+    self.moduleName = "EasyFlyer"
+    // You can add your custom initial props here
+    // self.initialProps = [:]
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+  
+  override func sourceURL(for bridge: RCTBridge) -> URL? {
+    return self.bundleURL()
+  }
+  
+  override func bundleURL() -> URL? {
+#if DEBUG
+    return RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+#else
+    return Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+#endif
+  }
+}
